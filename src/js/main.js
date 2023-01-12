fde3b996390e526388d4cca61a59da09d3bc8287
Romik1999 <usankin.roma@gmail.com>
2023-01-12 16:13:18 +0400

    finished html

--- src/js/main.js
+++ src/js/main.js
@@ -23,4 +23,29 @@ $(function () {
             },
         },
     });
+    function digits_int(target){
+        let val1 = $(target).val().replace(/[^0-9]/g, '');
+        let val =val1.replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+        $(target).val(val);
+    }
+
+    digits_int('.amount-show');
+
+    var $element = $('input[type="range"]');
+    $element
+        .rangeslider({
+            polyfill: false,
+            onInit: function () {
+                $('.amount-show').val(this.value.toLocaleString('ru') + ' Р');
+            }
+        })
+        .on('input', function (e) {
+            let rangeVAl = this.value.replace(/[^0-9]/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+            $('.amount-show').val(rangeVAl + ' Р')
+        });
+
+
+    $('.popup__close').click(function () {
+        $('.carousel__button.is-close').click();
+    });
 });
