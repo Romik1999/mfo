0f38f952d0e46a1256966329b7ebb8eff511e8b7
roman <usankin.roma@gmail.com>
2023-01-15 22:28:42 +0400

    finished modals

--- src/js/main.js
+++ src/js/main.js
@@ -91,6 +91,61 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+
+
+
+    // функция для модалки
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scarollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scarollWidth;
+    }
+
+    let scrollWidth = calcScroll();
+
+    function modal(modal, modalActiveClass, triggers, modalClose) {
+        const triggers_ = document.querySelectorAll(triggers),
+            modal_ = document.querySelector(modal),
+            modalClose_ = document.querySelector(modalClose);
+
+        if (triggers_.length > 0) {
+            triggers_.forEach(item => {
+                item.addEventListener('click', () => {
+                    modal_.classList.add(modalActiveClass);
+                    document.body.style.overflow = 'hidden';
+                    document.body.style.marginRight = `${scrollWidth}px`;
+                });
+            });
+
+            modalClose_.addEventListener('click', () => {
+                modal_.classList.remove(modalActiveClass);
+                document.body.style.overflow = '';
+                document.body.style.marginRight = '0px';
+            });
+
+            modal_.addEventListener('click', (e) => {
+                if (e.target.classList.contains('modal__container')) {
+                    modal_.classList.remove(modalActiveClass);
+                    document.body.style.overflow = '';
+                    document.body.style.marginRight = '0px';
+                }
+            });
+        }
+    }
+
+    modal('.modal-main', 'modal--active', '[data-modal="main"]', '.modal-main__close');
+    modal('.modal-reviews', 'modal--active', '[data-modal="reviews"]', '.modal-reviews__close');
+
     function maskPhone(e) {
         const mask = /\+7 \(\d{3}\) \d{3} \d{2} \d{2}/;
         var valSize = e.target.value.trim().replace(/\D/g, "").length;
